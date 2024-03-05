9a831cd74d8faf5e896fde9b82b563b8248a6876
NoahVanSteenlandt <noah.vansteenlandt@student.vives.be>
2024-03-05 22:46:03 +0100

    init firebaseConfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import BaseLayout from "@/layout/BaseLayout";
+import FirebaseData from "../components/firebasedata";
 
 export default function Home() {
   return (
     <BaseLayout>
       <p>test</p>
+      <FirebaseData></FirebaseData>
     </BaseLayout>
   );
 }

fa89dfef30d8b8fdd51fb685edd6bf1a151a8432
Cedric Depre <cedric.depre@student.vives.be>
2024-03-07 15:57:01 +0100

    created a form to create a seizoen

---
+++ src/app/admin/seizoen/CreateSeizoen/page.js
@@ -0,0 +1,9 @@
+import BaseLayout from "@/layout/BaseLayout";
+import SeizoenForm from "@/components/admin/seizoen/CreateSeizoenForm";
+export default function Home(){
+    return(
+        <BaseLayout>
+            <SeizoenForm></SeizoenForm>
+        </BaseLayout>
+    )
+}

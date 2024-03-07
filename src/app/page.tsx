aedef8f9f83d9fc4085ab3e330d119db222f47e3
Daniil Samsonov <daniil.samsonov@student.vives.be>
2024-03-07 14:19:00 +0100

    commit voor merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import BaseLayout from "@/layout/BaseLayout";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <BaseLayout>
       <p>test</p>
+      <Link href={"/forgotPassword"}>Forgot</Link>
     </BaseLayout>
   );
 }

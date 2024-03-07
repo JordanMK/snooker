bb7c455ccb3f21d8148a20fea20910aaf2ae9313
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-03-07 15:44:45 +0100

    basic header, nav not working yet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,17 @@
+import "./components.css"
 import React from "react";
-import Link from "next/link";
-// import Logo from "./Logo";
-import Button from "./Button";
-import css from "../app/page.module.css"
+import "../app/page"
 
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            {/* <Logo /> */}
-            <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/about">
-                  <p>About Us</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services">
-                  <p>Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contacts">
-                  <p>Contacts</p>
-                </Link>
-              </li>
-            </ul>
-            <Button />
+        <div className="header">
+          <a href="#index" className="logo">Snooker Pocket</a>
+          <div className="header-right">
+            <a className="active" href="../app/page">Home</a>
+            <a href="../app/admin/page">Admin page</a>
           </div>
         </div>
-      </div>
     </>
   );
 };

4ec311b2b5d255cb99372b652f22171e1bc87bbb
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-03-07 14:35:36 +0100

    v1 footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,18 @@
-import '../app/page.module.css'
-
-
 export default function Footer() {
     return <>
         <footer className="site-footer" style={{
-              backgroundColor:'#26272b',
-              padding: "45px",
-  fontSize: "15px",
-  lineHeight: "24px",
-  color: "#737373",
-  boxShadow: "0 10px 20px rgba(0,0,0,0.5)",
+                backgroundColor:'#1f2121',
+                padding: "10px",
+                fontSize: "15px",
+                lineHeight: "24px",
+                color: "#e8ebea",
+                boxShadow: "0 10px 20px rgba(0,0,0,0.5)",
         }}>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-8 col-sm-6 col-12">
-                        <p className="copyright-text">Copyright © 2020 All Rights Reserved by
-                            <a href="#"><span className="logo"> VIVES.</span></a>
+                    <div className="col-md-6 col-sm-6 col-12">
+                        <p className="copyright-text">Copyright © 2024 All Rights Reserved by 
+                            <a href="https://www.vives.be/nl"><span className="logo">VIVES.</span></a>
                         </p>
                     </div>
                 

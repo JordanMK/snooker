fda6dadd8b27e39609d3d9b54e3676fa926d2a27
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-03-07 15:24:01 +0100

    trying to fix tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 // import Logo from "./Logo";
 import Button from "./Button";
+import css from "../app/page.module.css"
 
 const Navbar = () => {
   return (

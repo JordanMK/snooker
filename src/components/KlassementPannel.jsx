4a15702b412f02c2ad9b69b2492b0bcdadee3386
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-03-13 17:25:39 +0100

    Klassement naast elkaar

--- src/components/KlassementPannel.jsx
+++ src/components/KlassementPannel.jsx
@@ -1,4 +1,5 @@
 import "../app/css/Klassement.css"
+
 export default function KlassementPannel({ use }) {
     // Check if user.users is an array
     if (!Array.isArray(use.users)) {
@@ -7,49 +8,52 @@ export default function KlassementPannel({ use }) {
   
     return (
       <>
-        <h1>Klassement Speeldag {use.userid}</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Plaats</th>
-              <th>Naam</th>
-              <th>Score</th>
-            </tr>
-          </thead>
-          <tbody>
-            {use.users.map((user) => (
-              <tr key={use.userid}>
-                <td>{user.plaats}</td>
-                <td>{user.naam}</td>
-                <td>{user.score}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <br />
-        <a className="a" href="">Show more</a>
-        <hr />
-        <h1>Klassement Seizoen </h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Plaats</th>
-              <th>Naam</th>
-              <th>Score</th>
-            </tr>
-          </thead>
-          <tbody>
-            {use.users.map((user) => (
-              <tr key={use.userid}>
-                <td>{user.plaats}</td>
-                <td>{user.naam}</td>
-                <td>{user.score}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <br />
-        <a className="a" href="">Show more</a>
+        <div className="pageContainer">
+          <div className="column smallColumn">
+            <h1>Klassement Speeldag {use.userid}</h1>
+            <h1>Klassement Seizoen </h1>
+            <div className="panelNav">
+            <table>
+              <thead>
+                <tr>
+                  <th>Plaats</th>
+                  <th>Naam</th>
+                  <th>Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {use.users.map((user) => (
+                  <tr key={use.userid}>
+                    <td>{user.plaats}</td>
+                    <td>{user.naam}</td>
+                    <td>{user.score}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <table>
+              <thead>
+                <tr>
+                  <th>Plaats</th>
+                  <th>Naam</th>
+                  <th>Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {use.users.map((user) => (
+                  <tr key={use.userid}>
+                    <td>{user.plaats}</td>
+                    <td>{user.naam}</td>
+                    <td>{user.score}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            {/* <a className="a" href="">Show more</a>
+            <a className="a" href="">Show more</a> */}
+            </div>
+          </div>
+        </div>
       </>
     );
     

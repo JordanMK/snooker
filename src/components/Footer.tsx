15cbac2b8a0b85e134f977ed3fd4d1c78067a287
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-03-13 23:39:37 +0100

    removed inline css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,23 +3,14 @@ import "./components.css";
 
 export default function Footer() {
     return <>
-        <footer className="site-footer" style={{
-                paddingLeft: "20px",
-                fontSize: "15px",
-                lineHeight: "24px",
-                color: "#000000",
-                position: "fixed",
-                bottom: "0",
-                width: "100%",
-        }}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 col-sm-6 col-12">
+        <footer>
+            <div >
+                <div >
+                    <div>
                         <p className="copyright-text">Copyright © 2024 All Rights Reserved by 
-                            <a href="https://www.vives.be/nl"><span className="logo"> VIVES.</span></a>
+                            <a href="https://www.vives.be/nl"> <span className="logo">VIVES.</span></a>
                         </p>
-                    </div>
-                
+                    </div>                
                 </div>
             </div>
         </footer>

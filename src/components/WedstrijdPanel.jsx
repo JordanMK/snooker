bd1307476083bd8fbe312e5827e37de760492d5d
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-03-14 16:08:35 +0100

    added prognosemogelijkheden

--- src/components/WedstrijdPanel.jsx
+++ src/components/WedstrijdPanel.jsx
@@ -2,19 +2,33 @@ export default function WedstrijdPanel ({speeldag}) {
     return (
         <>
             <h1>Speeldag {speeldag.speeldagNr}</h1>
-            <ul>
+            <table style={{width: "100%"}}>
+                <tr>
+                    <th>match</th>
+                    <th>Winst ploeg 1</th>
+                    <th>Gelijkspel</th>
+                    <th>Winst ploeg 2</th>
+                </tr>
                 {speeldag.wedstrijden.map(wedstrijd =>
-                        <li key={wedstrijd.id}>
-                            <span>{wedstrijd.thuis}</span>
-                            <span> VS </span>
-                            <span>{wedstrijd.weg}</span>
-                            <span> --- </span>
-                            <input type="checkbox"/>
-                            <span> VS </span>
-                            <input type="checkbox"/>
-                        </li>
+                        <tr key={wedstrijd.id}>
+                            <td>
+                                <span>{wedstrijd.thuis}</span>
+                                <span> - </span>
+                                <span>{wedstrijd.weg}</span>
+                            </td>
+                            <td>
+                                <input type="checkbox"/>   
+                            </td>
+                            <td>
+                                <input type="checkbox"/>   
+                            </td>
+                            <td>
+                                <input type="checkbox"/>   
+                            </td>
+                                                      
+                        </tr>
                 )}
-            </ul>
+            </table>
             <span>Joker?</span><input type="checkbox"/>
             <h3>Schiftingsvraag:</h3>
             <p>Gok nummer 1 tot 10000</p>

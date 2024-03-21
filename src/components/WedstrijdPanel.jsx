0242da869676753f45e36c7eb5aa8b8ae0153743
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-03-21 16:34:03 +0100

    added api

--- src/components/WedstrijdPanel.jsx
+++ src/components/WedstrijdPanel.jsx
@@ -1,10 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import "./components.css";
 
-export default function WedstrijdPanel ({speeldag}) {
+export default function WedstrijdPanel({ speeldag }) {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/users/')
+            .then(response => response.json())
+            .then(json => setData(json))
+            .catch(error => console.error(error));
+    }, []);
+
     return (
         <>
+            <div>
+                {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+            </div>
+
             <p className="speeldagTitel">Speeldag {speeldag.speeldagNr}</p>
-            <table style={{width: "100%"}}>
+            <table style={{ width: "100%" }}>
                 <tr>
                     <th>match</th>
                     <th>Winst ploeg 1</th>
@@ -12,39 +27,39 @@ export default function WedstrijdPanel ({speeldag}) {
                     <th>Winst ploeg 2</th>
                 </tr>
                 {speeldag.wedstrijden.map(wedstrijd =>
-                        <tr key={wedstrijd.id}>
-                            <td>
-                                <span>{wedstrijd.id}. </span>
-                                <span>{wedstrijd.thuis}</span>
-                                <span> - </span>
-                                <span>{wedstrijd.weg}</span>
-                            </td>
-                            <td>
-                                <input type="checkbox"/>   
-                            </td>
-                            <td>
-                                <input type="checkbox"/>   
-                            </td>
-                            <td>
-                                <input type="checkbox"/>   
-                            </td>
-                                                      
-                        </tr>
+                    <tr key={wedstrijd.id}>
+                        <td>
+                            <span>{wedstrijd.id}. </span>
+                            <span>{wedstrijd.thuis}</span>
+                            <span> - </span>
+                            <span>{wedstrijd.weg}</span>
+                        </td>
+                        <td>
+                            <input type="checkbox" name="check" />
+                        </td>
+                        <td>
+                            <input type="checkbox" name="check" />
+                        </td>
+                        <td>
+                            <input type="checkbox" name="check" />
+                        </td>
+
+                    </tr>
                 )}
             </table>
             <div className="jokerContainer checkbox-wrapper-13">
                 <label htmlFor="c1-13">Gebruik joker?</label>
-                <input type="checkbox" name="joker" id="c1-13"/>
-                
+                <input type="checkbox" name="joker" id="c1-13" />
+
             </div>
-            
+
             <div className="schiftingsContainer">
                 <h4>Schiftingsvraag:</h4>
 
                 <label htmlFor="schiftingsvraag">Gok nummer 1 tot 10000: </label>
-                <input type="number" min="0" name="schiftingsvraag" id="schiftingsvraag"/>
+                <input type="number" min="0" name="schiftingsvraag" id="schiftingsvraag" />
             </div>
-            
+
         </>
     );
 }

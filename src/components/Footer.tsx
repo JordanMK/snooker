1727d0f954abfbaab2ff3a598719895141586dc5
Justas <justas.valutis@student.vives.be>
2024-03-21 16:26:59 +0100

    footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,46 @@ import "./components.css";
 
 export default function Footer() {
     return <>
-        <footer>
-            <div >
-                <div >
-                    <div>
-                        <p className="copyright-text">Copyright © 2024 All Rights Reserved by 
-                            <a href="https://www.vives.be/nl"> <span className="logo">VIVES.</span></a>
-                        </p>
-                    </div>                
+ <footer>
+                <div className="">
+                    <div className="">
+                        <div className="">
+                            <div className="">
+                                <img src="../images/snooker.png" width="110px" alt="Snooker Pocket uit Deerlijk"/>
+                            </div>
+                            <div className="uk-width-1-3@s uk-margin-top-remove@m uk-margin-top">
+                                <h3>Snooker Pocket</h3>
+                                <p>Hoogstraat 78</p>
+                                <p>8540 Deerlijk</p>
+                                <p>
+                                    <a href="tel:0473 79 73 50">0473 79 73 50</a>
+                                </p>
+
+                            </div>
+                            <div className="uk-width-1-3@s uk-margin-top-remove@m uk-margin-top">
+                                <h3>Volg ons</h3>
+                                <p>
+                                    Via onze 
+                                    <a href="https://www.facebook.com/Snooker-Pocket-790776524369943" target="_blank">facebookpagina</a>
+                                    houden we je op de hoogte van alle updates en nieuwtjes 
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </footer>
+                <div className="subfooter">
+                    <div className="uk-container">
+                        <div className="uk-grid">
+                            <div className="uk-width-1-2@s uk-text-left@s">
+                                <p>© 2020 Snooker Pocket</p>
+                            </div>
+                            <div className="uk-width-1-2@s uk-text-right@s">
+                                <p>Designed by <a href="https://eagl.be" target="_blank">Eagl</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
     </>
 }

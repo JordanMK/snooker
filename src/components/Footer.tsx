26e3a72ade14f1ed824698867505e4f210378093
Justas <justas.valutis@student.vives.be>
2024-03-21 17:52:03 +0100

    werken verder aan footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,50 +4,47 @@ import snooker from '../images/snooker.png'
 
 export default function Footer() {
     return <>
- <footer>
+        <footer>
+            <div className="">
                 <div className="">
-                    <div className="">
-                        <div className="">
-                            <div className="">
+                    <div className="footerFlex">
+                        <div className="imageFooter">
                             <Image
-      src={snooker}
-      width={110}
-      alt="Picture of the author"
-    />
-                            </div>
-                            <div className="uk-width-1-3@s uk-margin-top-remove@m uk-margin-top">
-                                <h3>Snooker Pocket</h3>
-                                <p>Hoogstraat 78</p>
-                                <p>8540 Deerlijk</p>
-                                <p>
-                                    <a href="tel:0473 79 73 50">0473 79 73 50</a>
-                                </p>
+                                src={snooker}
+                                width={110}
+                                alt="Picture of the author"
+                            />
+                        </div>
+                        <div className="">
+                            <h3>Snooker Pocket</h3>
+                            <p>Hoogstraat 78</p>
+                            <p>8540 Deerlijk</p>
+                            <p>
+                                <a href="tel:0473 79 73 50">0473 79 73 50</a>
+                            </p>
 
-                            </div>
-                            <div className="uk-width-1-3@s uk-margin-top-remove@m uk-margin-top">
-                                <h3>Volg ons</h3>
-                                <p>
-                                    Via onze 
-                                    <a href="https://www.facebook.com/Snooker-Pocket-790776524369943" target="_blank">facebookpagina</a>
-                                    houden we je op de hoogte van alle updates en nieuwtjes 
-                                </p>
+                        </div>
+                        <div className="">
+                            <h3>Volg ons</h3>
+                            <p className="textFooter">
+                                Via onze
+                                <a href="https://www.facebook.com/Snooker-Pocket-790776524369943" target="_blank"> facebookpagina </a>
+                                houden we je op de hoogte van alle updates en nieuwtjes
+                            </p>
 
-                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="subfooter">
-                    <div className="uk-container">
-                        <div className="uk-grid">
-                            <div className="uk-width-1-2@s uk-text-left@s">
-                                <p>© 2020 Snooker Pocket</p>
-                            </div>
-                            <div className="uk-width-1-2@s uk-text-right@s">
-                                <p>Designed by <a href="https://eagl.be" target="_blank">Eagl</a></p>
-                            </div>
+            </div>
+            <div className="subfooter">
+                <div className="uk-container">
+                    <div className="uk-grid">
+                        <div className="uk-width-1-2@s uk-text-left@s">
+                            <p>© 2020 Snooker Pocket</p>
                         </div>
                     </div>
                 </div>
-            </footer>
+            </div>
+        </footer>
     </>
 }

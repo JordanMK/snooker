744036ff81c5e7667d300ebe71ed07193ab662b1
Daniil Samsonov <daniil.samsonov@student.vives.be>
2024-03-21 17:15:33 +0100

    opmaak

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,68 @@
 import "./components.css";
-import Image from 'next/image'
-import snooker from '../images/snooker.png'
+import footer from './footer.module.css'
+import Image from "next/image";
+import snooker from "../images/snooker.png";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Footer() {
-    return <>
- <footer>
-                <div className="">
-                    <div className="">
-                        <div className="">
-                            <div className="">
-                            <Image
-      src={snooker}
-      width={110}
-      alt="Picture of the author"
-    />
-                            </div>
-                            <div className="uk-width-1-3@s uk-margin-top-remove@m uk-margin-top">
-                                <h3>Snooker Pocket</h3>
-                                <p>Hoogstraat 78</p>
-                                <p>8540 Deerlijk</p>
-                                <p>
-                                    <a href="tel:0473 79 73 50">0473 79 73 50</a>
-                                </p>
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="d-flex align-content-center">
 
-                            </div>
-                            <div className="uk-width-1-3@s uk-margin-top-remove@m uk-margin-top">
-                                <h3>Volg ons</h3>
-                                <p>
-                                    Via onze 
-                                    <a href="https://www.facebook.com/Snooker-Pocket-790776524369943" target="_blank">facebookpagina</a>
-                                    houden we je op de hoogte van alle updates en nieuwtjes 
-                                </p>
+                <div className="float-left">
+                    <Image src={snooker} width={110} alt="Picture of the author" />
+                </div>
 
-                            </div>
-                        </div>
-                    </div>
+            <div className="">
+                <div>
+                <h3>Snooker Pocket</h3>
+                <p>Hoogstraat 78</p>
+                <p>8540 Deerlijk</p>
+                <p>
+                  <a href="tel:0473 79 73 50">0473 79 73 50</a>
+                </p>
                 </div>
-                <div className="subfooter">
-                    <div className="uk-container">
-                        <div className="uk-grid">
-                            <div className="uk-width-1-2@s uk-text-left@s">
-                                <p>© 2020 Snooker Pocket</p>
-                            </div>
-                            <div className="uk-width-1-2@s uk-text-right@s">
-                                <p>Designed by <a href="https://eagl.be" target="_blank">Eagl</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            </div>
+            
+              
+              <div className="float-right">
+                <h3>Volg ons</h3>
+                <p>
+                  Via onze
+                  <a
+                    href="https://www.facebook.com/Snooker-Pocket-790776524369943"
+                    target="_blank"
+                  >
+                    facebookpagina
+                  </a>
+                  houden we je op de hoogte van alle updates en nieuwtjes
+                </p>
+              </div>
+              </div>
+            
+          
+        
+        <div className="subfooter">
+          <div className="uk-container">
+            <div className="uk-grid">
+              <div className="uk-width-1-2@s uk-text-left@s">
+                <p>© 2020 Snooker Pocket</p>
+              </div>
+              <div className="uk-width-1-2@s uk-text-right@s">
+                <p>
+                  Designed by{" "}
+                  <a href="https://eagl.be" target="_blank">
+                    Eagl
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </footer>
     </>
+  );
 }

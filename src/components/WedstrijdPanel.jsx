c2bc9cfea24b6548fd18ab019db458a0fe39fbf2
ThijsGeeraert2 <thijs.geeraert@student.vives.be>
2024-03-22 14:56:13 +0100

    api working v1

--- src/components/WedstrijdPanel.jsx
+++ src/components/WedstrijdPanel.jsx
@@ -20,46 +20,50 @@ export default function WedstrijdPanel({ speeldag }) {
     return (
         <>
             <div>
-            {data ? 
-                data.map(match => (
-                    <div key={match._id}>
-                        <pre>{JSON.stringify(match, null, 2)}</pre>
-                        <p>{match.resultaat}</p>
-                    </div>
-                ))
-                : 'Loading...'}
-                
+                {data ?
+                    data.map(match => (
+                        <div key={match._id}>
+                            <pre>{JSON.stringify(match, null, 2)}</pre>
+                            <p>{match.resultaat}</p>
+                        </div>
+                    ))
+                    : 'Loading...'}
+
             </div>
 
-            <p className="speeldagTitel">Speeldag {speeldag.speeldagNr}</p>
-            <table style={{ width: "100%" }}>
-                <tr>
-                    <th>match</th>
-                    <th>Winst ploeg 1</th>
-                    <th>Gelijkspel</th>
-                    <th>Winst ploeg 2</th>
-                </tr>
-                {speeldag.wedstrijden.map(wedstrijd =>
-                    <tr key={wedstrijd.id}>
-                        <td>
-                            <span>{wedstrijd.id}. </span>
-                            <span>{wedstrijd.thuis}</span>
-                            <span> - </span>
-                            <span>{wedstrijd.weg}</span>
-                        </td>
-                        <td>
-                            <input type="checkbox" name="check" />
-                        </td>
-                        <td>
-                            <input type="checkbox" name="check" />
-                        </td>
-                        <td>
-                            <input type="checkbox" name="check" />
-                        </td>
-
+            <div>
+                <p className="speeldagTitel">Speeldag {speeldag.speeldagNr}</p>
+                <table style={{ width: "100%" }}>
+                    <tr>
+                        <th>match</th>
+                        <th>Winst ploeg 1</th>
+                        <th>Gelijkspel</th>
+                        <th>Winst ploeg 2</th>
                     </tr>
-                )}
-            </table>
+                    {data ? data.map(match => (
+                        <tr key={match._id}>
+                            <td>
+                                <span>{match._id}. </span>
+                                <span>{match.thuis}</span>
+                                <span> - </span>
+                                <span>{match.uit}</span>
+                            </td>
+                            <td>
+                                <input type="checkbox" name="check" />
+                            </td>
+                            <td>
+                                <input type="checkbox" name="check" />
+                            </td>
+                            <td>
+                                <input type="checkbox" name="check" />
+                            </td>
+
+                        </tr>
+                    )) 
+                    : 'Loading...'}
+                </table>
+            </div>
+
             <div className="jokerContainer checkbox-wrapper-13">
                 <label htmlFor="c1-13">Gebruik joker?</label>
                 <input type="checkbox" name="joker" id="c1-13" />

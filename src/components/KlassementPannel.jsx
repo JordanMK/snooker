37158632eedf4d8052b3005630639703fa78e16d
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-03-25 19:00:49 +0100

    klassement weergeven via API

--- src/components/KlassementPannel.jsx
+++ src/components/KlassementPannel.jsx
@@ -1,18 +1,34 @@
 import React, { useState, useEffect } from "react";
-import getUsers from "../components/api_calls/call";
+import { getSpeeldagen, getKlassement, getUserName, getSeizoenen } from "./api_calls/call.js";
 import "../app/css/Klassement.css";
 
 export default function KlassementPannel() {
-  const [users, setUsers] = useState([]);
+  const [speeldagen, setSpeeldagen] = useState([]);
+  const [seizoenen, setSeizoenen] = useState([]);
+  const [klassement, setKlassement] = useState([]);
+  const [user, setUser] = useState({
+    username: "",
+  });
+
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await getUsers();
-      setUsers(JSON.parse(result));
-    };
-
-    fetchData();
+    getSpeeldagen()
+      .then((speeldagen) => {
+        setSpeeldagen(speeldagen);
+        return getKlassement(speeldagen[0]._id);
+      })
+      .then((klassement) => {
+        setKlassement(klassement);
+        return getUserName(klassement[0].user);
+      })
+      .then((user) => {
+        setUser(user);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
   }, []);
+
 
   return (
     <>
@@ -31,27 +47,11 @@ export default function KlassementPannel() {
                 </tr>
               </thead>
               <tbody>
-                {users.map((user) => (
-                  <tr key={user.plaats}>
-                    <td>{user.plaats}</td>
-                    <td>{user.naam}</td>
-                    <td>{user.score}</td>
-                    {/*<table className="score">
-                        <thead>
-                          <tr>
-                            <th>Wedstrijd</th>
-                            <th>Score</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {user.wed.map((wedstrijd) => (
-                            <tr key={wedstrijd.nr}>
-                              <td>{wedstrijd.nr}</td>
-                              <td>{wedstrijd.scores}</td>
-                            </tr>
-                          ))}
-                        </tbody>
-                          </table>*/}
+                {klassement.map((item) => (
+                  <tr key={item._id}>
+                    <td>{item.plaats}</td>
+                    <td>{user.username}</td>
+                    <td>{item.score}</td>
                   </tr>
                 ))}
               </tbody>
@@ -66,27 +66,11 @@ export default function KlassementPannel() {
                 </tr>
               </thead>
               <tbody>
-                {users.map((user) => (
-                  <tr key={user.plaats}>
-                    <td>{user.plaats}</td>
-                    <td>{user.naam}</td>
-                    <td>{user.score}</td>
-                    {/*<table className="score">
-                        <thead>
-                          <tr>
-                            <th>Wedstrijd</th>
-                            <th>Score</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {user.wed.map((wedstrijd) => (
-                            <tr key={wedstrijd.nr}>
-                              <td>{wedstrijd.nr}</td>
-                              <td>{wedstrijd.scores}</td>
-                            </tr>
-                          ))}
-                        </tbody>
-                          </table>*/}
+                {klassement.map((item) => (
+                  <tr key={item._id}>
+                    <td>{item.plaats}</td>
+                    <td>{user.username}</td>
+                    <td>{item.score}</td>
                   </tr>
                 ))}
               </tbody>
--- src/components/api_calls/call.js
+++ src/components/api_calls/call.js
@@ -4,8 +4,9 @@ const port = 3001;
 const speeldagenUrl = `${base_url}:${port}/api/speeldagen`;
 const klassementUrl = `${base_url}:${port}/api/speeldagen/`;
 const usersUrl = `${base_url}:${port}/api/users/`;
+const seizoenenUrl = `${base_url}:${port}/api/seizoenen`;
 
-function getSpeeldagen() {
+export function getSpeeldagen() {
   return new Promise((resolve, reject) => {
     const request = get(speeldagenUrl);
     request.on('response', (response) => {
@@ -28,7 +29,30 @@ function getSpeeldagen() {
   });
 }
 
-function getKlassement(id) {
+export function getSeizoenen() {
+  return new Promise((resolve, reject) => {
+    const request = get(seizoenenUrl);
+    request.on('response', (response) => {
+      if (response.statusCode === 200) {
+        let data = '';
+        response.on('data', (chunk) => {
+          data += chunk;
+        });
+        response.on('end', () => {
+          const seizoenen = JSON.parse(data);
+          resolve(seizoenen);
+        });
+      } else {
+        reject(new Error('Failed to retrieve speeldagen'));
+      }
+    });
+    request.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
+
+export function getKlassement(id) {
   return new Promise((resolve, reject) => {
     const request = get(`${klassementUrl}${id}/klassement`);
     request.on('response', (response) => {
@@ -51,7 +75,7 @@ function getKlassement(id) {
   });
 }
 
-function getUserName(id){
+export function getUserName(id){
   return new Promise((resolve, reject) => {
     const request = get(`${usersUrl}${id}`);
     request.on('response', (response) => {
@@ -73,20 +97,3 @@ function getUserName(id){
     });
   });
 }
-
-// usage
-getSpeeldagen()
-  .then(speeldagen => {
-    return getKlassement(speeldagen[0]._id);
-  })
-  .then(klassement => {
-    console.log(klassement[0].user);
-    console.log(`Score: ${klassement[0].score}`);
-    return getUserName(klassement[0].user)
-  })
-  .then(user => {
-    console.log(`User: ${user.username}`);
-  })
-  .catch(error => {
-    console.error(error.message);
-  });

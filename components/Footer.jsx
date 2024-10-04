89aaa9e2f26a2312ce5974c349d855c28a95d3b2
Arthur0x1 <115065914+Arthur0x1@users.noreply.github.com>
2024-10-04 21:44:57 +0200

    Add extra padding to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,43 +3,47 @@ import "./components.css";
 import Image from 'next/image'
 import snooker from "../public/snooker.png"
 
-export default function Footer() {
-    return <>
-        <footer>
-            <div className="footerFlex">
-                <div className="imageFooter">
-                    <Image
-                        src={snooker}
-                        width={110}
-                        alt="Picture of the author"
-                    />
-                </div>
-                <div id="address">
-                    <h3>Snooker Pocket</h3>
-                    <p>Hoogstraat 78</p>
-                    <p>8540 Deerlijk</p>
-                    <p>
-                        <a href="tel:0473 79 73 50">0473 79 73 50</a>
-                    </p>
+const TELEPHONE = "0473 79 73 50"
+const EDGES_PADDING = 18
 
-                </div>
-                <div className="">
-                    <h3>Volg ons</h3>
-                    <span>
-                      Via onze <a href="https://www.facebook.com/Snooker-Pocket-790776524369943" target="_blank">facebookpagina</a> houden we je
-                      op de hoogte van alle updates en nieuwtjes
-                    </span>
-                </div>
-            </div>
-            <div className="subfooter">
-                <div className="uk-container">
-                    <div className="uk-grid">
-                        <div className="uk-width-1-2@s uk-text-left@s">
-                            <p>© 2020 Snooker Pocket</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </>
+export default function Footer() {
+  return <footer>
+    <div className="d-flex justify-content-between">
+
+      <section className="d-flex justify-content-center align-items-center" style={{paddingLeft: EDGES_PADDING}}>
+        <Image
+          src={snooker}
+          width={110}
+          alt="Picture of the author"
+        />
+      </section>
+
+      <section id="address">
+        <h3>Snooker Pocket</h3>
+        <p>Hoogstraat 78</p>
+        <p>8540 Deerlijk</p>
+        <a href={`tel:${TELEPHONE}`}>{TELEPHONE}</a>
+      </section>
+
+      <div style={{paddingRight: EDGES_PADDING}}>
+        <h3>Volg ons</h3>
+        <span>
+          Via onze <a href="https://www.facebook.com/Snooker-Pocket-790776524369943" target="_blank">facebookpagina</a> houden we je
+          op de hoogte van alle updates en nieuwtjes
+        </span>
+      </div>
+
+    </div>
+
+    <section className="subfooter">
+      <div className="uk-container">
+        <div className="uk-grid">
+          <div className="uk-width-1-2@s uk-text-left@s">
+            <p>© 2020 Snooker Pocket</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </footer>
 }

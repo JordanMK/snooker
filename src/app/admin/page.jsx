73ab3956c079493c8ea997615bb1f22b4af01074
Arthur0x1 <115065914+Arthur0x1@users.noreply.github.com>
2024-10-04 22:27:25 +0200

    Move admin dashboard to its own page

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,29 @@
+"use client"
+import LijstSeizoen from "../../../components/admin/LijstSeizoen"
+import Users from "../../../components/admin/users/users"
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const isAdmin = localStorage.getItem('admin')
+    console.log(isAdmin)
+    if (isAdmin === 'false') {
+      router.push('/')
+    }
+  }, [])
+
+  return (
+    <>
+      <div className="header">
+        <h1>Dashboard Admin</h1>
+      </div>
+      <div className="container">
+        <LijstSeizoen></LijstSeizoen>
+        <Users></Users>
+      </div>
+      </>
+  )
+}

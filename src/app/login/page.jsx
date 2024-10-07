ddb32c534ca26d4cee84899cfad1bf25a8dc6e30
Tuur Keersebilck <tuur.keersebilck@student.vives.be>
2024-10-07 11:17:01 +0200

    Register page fix

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,85 +1,85 @@
-"use client"
-import React, { useState } from "react"
-import Link from "next/link"
-import "./Login.css"
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import "./Login.css";
 
 export default function Login() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [emailError, setEmailError] = useState("")
-    const [passwordError, setPasswordError] = useState("")
-    const [loginFailed, setLoginFailed] = useState("")
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [emailError, setEmailError] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+	const [loginFailed, setLoginFailed] = useState("");
 
-    const formSubmit = async (e) => {
-        e.preventDefault() // Prevent default form submission
+	const formSubmit = async (e) => {
+		e.preventDefault(); // Prevent default form submission
 
-        if (!email) {
-            setEmailError("Email is required")
-            return
-        }
-        if (!password) {
-            setPasswordError("Password is required")
-            return
-        }
+		if (!email) {
+			setEmailError("Email is required");
+			return;
+		}
+		if (!password) {
+			setPasswordError("Password is required");
+			return;
+		}
 
-        try {
-            const response = await fetch("http://localhost:3001/api/auth/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email: email.toLowerCase(), password }), // Convert email to lowercase
-            })
+		try {
+			const response = await fetch("http://localhost:3001/api/auth/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email: email.toLowerCase(), password }), // Convert email to lowercase
+			});
 
-            if (response.ok) {
-                const data = await response.json()
-                localStorage.setItem("userID", data.user._id)
-                localStorage.setItem("userMail", data.user.email)
-                localStorage.setItem("admin", data.user.admin)
+			if (response.ok) {
+				const data = await response.json();
+				localStorage.setItem("userID", data.user._id);
+				localStorage.setItem("userMail", data.user.email);
+				localStorage.setItem("admin", data.user.admin);
 
-                window.location.href = "/"
-            } else {
-                setLoginFailed("Geef het juiste e-mailadres of wachtwoord")
-                console.log("Login NIET GELUKT")
-            }
-        } catch (error) {
-            setLoginFailed("Geen reactie van de server")
-            console.error("Error:", error)
-        }
-    }
+				window.location.href = "/";
+			} else {
+				setLoginFailed("Geef het juiste e-mailadres of wachtwoord");
+				console.log("Login NIET GELUKT");
+			}
+		} catch (error) {
+			setLoginFailed("Geen reactie van de server");
+			console.error("Error:", error);
+		}
+	};
 
-    return (
-        <form onSubmit={formSubmit} className="mainContainer">
-            {loginFailed && <label className="errorLabel">{loginFailed}</label> }
-            <div className="titleContainer">
-                <div>Login</div>
-            </div>
-            <br />
-            <input
-                value={email}
-                placeholder="Typ je e-mail"
-                onChange={(e) => setEmail(e.target.value)}
-                className="inputBox"
-                type="email"
-            />
-            {emailError && <label className="errorLabel">{emailError}</label>}
-            <br />
-            <input
-                value={password}
-                placeholder="Typ je wachtwoord"
-                onChange={(e) => setPassword(e.target.value)}
-                className="inputBox"
-                type="password"
-            />
-            {passwordError && <label className="errorLabel">{passwordError}</label>}
-            <br />
-            <button className="button" type="submit">
-                Login
-            </button>
-            <br />
-            <div>
-                Heb je nog geen account? <Link href="/Register">Registreer hier</Link>
-            </div>
-        </form>
-    )
+	return (
+		<form onSubmit={formSubmit} className="mainContainer">
+			{loginFailed && <label className="errorLabel">{loginFailed}</label>}
+			<div className="titleContainer">
+				<div>Login</div>
+			</div>
+			<br />
+			<input
+				value={email}
+				placeholder="Typ je e-mail"
+				onChange={(e) => setEmail(e.target.value)}
+				className="inputBox"
+				type="email"
+			/>
+			{emailError && <label className="errorLabel">{emailError}</label>}
+			<br />
+			<input
+				value={password}
+				placeholder="Typ je wachtwoord"
+				onChange={(e) => setPassword(e.target.value)}
+				className="inputBox"
+				type="password"
+			/>
+			{passwordError && <label className="errorLabel">{passwordError}</label>}
+			<br />
+			<button className="button" type="submit">
+				Login
+			</button>
+			<br />
+			<div>
+				Heb je nog geen account? <Link href="/register">Registreer hier</Link>
+			</div>
+		</form>
+	);
 }
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,109 +1,115 @@
-"use client"
-import React, { useState } from "react"
+"use client";
+import React, { useState } from "react";
 // TODO: use a shared stylesheet
-import "../login/Login.css"
-import Link from "next/link"
+import "../login/Login.css";
+import Link from "next/link";
 
 export default function Register() {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  // TODO: setEmailError isnt used
-  const [emailError, setEmailError] = useState("")
-  const [password, setPassword] = useState("")
-  const [confirmPassword, setConfirmPassword] = useState("")
-  const [passwordError, setPasswordError] = useState(true)
-  const [registratieFailed, setRegistratieFailed] = useState("")
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	// TODO: setEmailError isnt used
+	const [emailError, setEmailError] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [passwordError, setPasswordError] = useState(true);
+	const [registratieFailed, setRegistratieFailed] = useState("");
 
-  const register = async (event) => {
-    event.preventDefault()
-    if (password !== confirmPassword || !password || !email || !username) {
-      setPasswordError(false)
-      setRegistratieFailed("Gegeven velden moeten ingevuld zijn")
-      return
-    }
+	const register = async (event) => {
+		event.preventDefault();
+		if (password !== confirmPassword || !password || !email || !username) {
+			setPasswordError(false);
+			setRegistratieFailed("Gegeven velden moeten ingevuld zijn");
+			return;
+		}
 
-    setRegistratieFailed(null)
-    setPasswordError(true)
+		setRegistratieFailed(null);
+		setPasswordError(true);
 
-    try {
-      const body = { admin: false, username, email: email.toLowerCase(), password }
-      const response = await fetch("http://localhost:3001/api/users/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body)
-      })
+		try {
+			const body = {
+				username,
+				email: email.toLowerCase(),
+				password,
+			};
+			const response = await fetch("http://localhost:3001/api/users/", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(body),
+			});
 
-      if (response.ok) {
-        // TODO: use
-        const data = await response.json()
-        window.location.href = "/login"
-      } else {
-        setRegistratieFailed("Dit gebruikersnaam of e-mailadres is bezet")
-        console.log("Registratie is NIET GELUKT")
-      }
-    } catch (error) {
-      setRegistratieFailed("Geen reactie van de server")
-      console.log("Error met de server")
-      console.error("Error:", error)
-    }
-  }
+			if (response.ok) {
+				// TODO: use
+				const data = await response.json();
+				window.location.href = "/login";
+			} else {
+				setRegistratieFailed("Dit gebruikersnaam of e-mailadres is bezet");
+				console.log("Registratie is NIET GELUKT");
+			}
+		} catch (error) {
+			setRegistratieFailed("Geen reactie van de server");
+			console.log("Error met de server");
+			console.error("Error:", error);
+		}
+	};
 
-  return (
-    <form onSubmit={register} className="mainContainer">
-      {registratieFailed && <label className="errorLabel">{registratieFailed}</label>}
-      <div className="titleContainer">
-        <div>Registreer</div>
-      </div>
-      <br />
-      <label htmlFor="username">Username</label>
-      <input
-        id="username"
-        value={username}
-        placeholder="username"
-        onChange={(e) => setUsername(e.target.value)}
-        className="inputBox"
-        type="text"
-      />
+	return (
+		<form onSubmit={register} className="mainContainer">
+			{registratieFailed && (
+				<label className="errorLabel">{registratieFailed}</label>
+			)}
+			<div className="titleContainer">
+				<div>Registreer</div>
+			</div>
+			<br />
+			<label htmlFor="username">Username</label>
+			<input
+				id="username"
+				value={username}
+				placeholder="username"
+				onChange={(e) => setUsername(e.target.value)}
+				className="inputBox"
+				type="text"
+			/>
 
-      <label htmlFor="email">Email</label>
-      <input
-        value={email}
-        placeholder="Geef je e-mailadres"
-        onChange={(e) => setEmail(e.target.value.toLowerCase())}
-        className="inputBox"
-        type="email"
-      />
-      {emailError && <label className="errorLabel">{emailError}</label>}
-      <label htmlFor="wachtwoord">Wachtwoord</label>
-      <input
-        style={{ borderColor: passwordError ? "grey" : "red" }}
-        id="wachtwoord"
-        value={password}
-        placeholder="Typ je wachtwoord"
-        onChange={(e) => setPassword(e.target.value)}
-        className="inputBox"
-        type="password"
-      />
-      <label htmlFor="wachtwoord2">Herhaal wachtwoord</label>
-      <input
-        style={{ borderColor: passwordError ? "grey" : "red" }}
-        id="wachtwoord2"
-        value={confirmPassword}
-        placeholder="Typ je wachtwoord"
-        onChange={(e) => setConfirmPassword(e.target.value)}
-        className="inputBox"
-        type="password"
-      />
-      <br />
-      <button className="button" type="submit">
-        Registreer
-      </button>
-      <br />
-      <div>
-        Terug naar de login pagina <Link href="/login">Klik hier</Link>
-      </div>
-    </form>
-  )
+			<label htmlFor="email">Email</label>
+			<input
+				value={email}
+				placeholder="Geef je e-mailadres"
+				onChange={(e) => setEmail(e.target.value.toLowerCase())}
+				className="inputBox"
+				type="email"
+			/>
+			{emailError && <label className="errorLabel">{emailError}</label>}
+			<label htmlFor="wachtwoord">Wachtwoord</label>
+			<input
+				style={{ borderColor: passwordError ? "grey" : "red" }}
+				id="wachtwoord"
+				value={password}
+				placeholder="Typ je wachtwoord"
+				onChange={(e) => setPassword(e.target.value)}
+				className="inputBox"
+				type="password"
+			/>
+			<label htmlFor="wachtwoord2">Herhaal wachtwoord</label>
+			<input
+				style={{ borderColor: passwordError ? "grey" : "red" }}
+				id="wachtwoord2"
+				value={confirmPassword}
+				placeholder="Typ je wachtwoord"
+				onChange={(e) => setConfirmPassword(e.target.value)}
+				className="inputBox"
+				type="password"
+			/>
+			<br />
+			<button className="button" type="submit">
+				Registreer
+			</button>
+			<br />
+			<div>
+				Terug naar de login pagina <Link href="/login">Klik hier</Link>
+			</div>
+		</form>
+	);
 }

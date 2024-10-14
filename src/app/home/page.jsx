79073f2e3e77be0c6863203f2d664f183d2de528
Arthur0x1 <115065914+Arthur0x1@users.noreply.github.com>
2024-10-14 20:27:45 +0200

    Make speeldagen on seizoen overview un-selectable

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -10,14 +10,19 @@ import { getSpeeldagen } from "@/src/api_calls"
 
 export default function Home() {
   const [leftPanelSelected, setLeftPanelSelected] = useState(true)
-  const [selectedSpeeldag, setselectedSpeeldag] = useState(0)
-  const [showklassementSeizoenPannel, setklassementSeizoenPannel] = useState(true)
+  const [selectedSpeeldag, setSelectedSpeeldag] = useState(null)
+  const [showklassementSeizoenPanel, setShowKlassementSeizoenPanel] = useState(true)
   const [speeldagen, setSpeeldagen] = useState([])
 
-  function onClickButton(i) {
-    setselectedSpeeldag(i)
-    setklassementSeizoenPannel(false)
-    setLeftPanelSelected(false)
+  const onSelectSpeeldag = (itemIdx) => {
+    if (selectedSpeeldag === itemIdx) {
+      setShowKlassementSeizoenPanel(true)
+      setSelectedSpeeldag(null)
+    } else {
+      setShowKlassementSeizoenPanel(false)
+      setSelectedSpeeldag(itemIdx)
+    }
+    //setLeftPanelSelected(false)
   }
 
   useEffect(() => {
@@ -27,36 +32,32 @@ export default function Home() {
   }, [])
 
   return (
-    <>
-
-      <div className="pageContainer">
-        <div className="smallColumn">
-          <SeizoenPanel onClick={onClickButton} speeldagen={speeldagen} />
+    <div className="pageContainer">
+      <div className="smallColumn">
+        <SeizoenPanel onClick={onSelectSpeeldag} speeldagen={speeldagen} />
+      </div>
+      <div className="column flexColumn">
+        <div className="panelNav">
+          <button onClick={() => setLeftPanelSelected(true)} style={{ backgroundColor: leftPanelSelected ? "#bc6c25" : "#dda15e" }}>
+            Klassement
+          </button>
+          <button onClick={() => setLeftPanelSelected(false)} style={{ backgroundColor: !leftPanelSelected ? "#bc6c25" : "#dda15e" }}>
+            Wedstrijd
+          </button>
         </div>
-        <div className="column flexColumn">
-          <div className="panelNav">
-            <button onClick={() => setLeftPanelSelected(true)} style={{ backgroundColor: leftPanelSelected ? "#bc6c25" : "#dda15e" }}>
-              Klassement
-            </button>
-            <button onClick={() => setLeftPanelSelected(false)} style={{ backgroundColor: !leftPanelSelected ? "#bc6c25" : "#dda15e" }}>
-              Wedstrijd
-            </button>
-          </div>
-          <div>
-            {console.log("speeldagen", speeldagen.seizoenID)}
-            {showklassementSeizoenPannel ? (
-              <KlassementSeizoenPannel seizoen_id={speeldagen.seizoenID} />
-            ) : (
-              <>
-                {leftPanelSelected ? (
-                  <KlassementPanel speeldag_id={speeldagen[selectedSpeeldag]._id} />
-                ) : <WedstrijdPanel speeldag_id={speeldagen[selectedSpeeldag]._id} />
-                }
-              </>
-            )}
-          </div>
+        <div>
+          {showklassementSeizoenPanel ? (
+            <KlassementSeizoenPannel seizoen_id={speeldagen.seizoenID} />
+          ) : (
+            <>
+              {leftPanelSelected ? (
+                <KlassementPanel speeldag_id={speeldagen[selectedSpeeldag]._id} />
+              ) : <WedstrijdPanel speeldag_id={speeldagen[selectedSpeeldag]._id} />
+              }
+            </>
+          )}
         </div>
       </div>
-    </>
+    </div>
   )
 }

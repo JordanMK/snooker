3bf4596725422407a0311105099bbf03347aac9a
Tuur Keersebilck <tuur.keersebilck@student.vives.be>
2024-10-15 15:50:06 +0200

    patch wedstrijd fix

--- components/admin/wedstrijd/PasWedstrijdAan.jsx
+++ components/admin/wedstrijd/PasWedstrijdAan.jsx
@@ -1,65 +1,82 @@
-import { Form, Button } from "react-bootstrap"
-import { patchWedstrijd } from "@/src/api_calls"
-import { useRouter } from "next/navigation"
+import { Form, Button } from "react-bootstrap";
+import { patchWedstrijd } from "@/src/api_calls";
+import { useRouter } from "next/navigation";
 
-export default function PasWedstrijdAan({ id, thuis, uit, datum, resultaat, seizoenId }) {
-  const router = useRouter()
+export default function PasWedstrijdAan({
+	id,
+	thuis,
+	uit,
+	datum,
+	resultaat,
+	seizoenId,
+}) {
+	const router = useRouter();
 
-  async function handlePatchSubmit(event) {
-    event.preventDefault()
+	async function handlePatchSubmit(event) {
+		event.preventDefault();
 
-    // TODO: why is resultaat both defined in props and retrieved from the form?
-    const formData = new FormData(event.target)
-    const [date, homeTeam, awayTeam, resultaat] =
-      ["date", "homeTeam", "awayTeam", "resultaat"]
-      .map(formData.get)
+		const formData = new FormData(event.target);
+		const [date, homeTeam, awayTeam, result] = [
+			"date",
+			"homeTeam",
+			"awayTeam",
+			"resultaat",
+		].map((field) => formData.get(field));
 
-    patchWedstrijd(date, homeTeam, awayTeam, resultaat, id, seizoenId)
-      .then(router.reload)
-      .catch(error => console.error("Failed to patch wedstrijd:", error.message))
-  }
+		patchWedstrijd(date, homeTeam, awayTeam, result, id, seizoenId)
+			.then(router.reload)
+			.catch((error) =>
+				console.error("Failed to patch wedstrijd:", error.message)
+			);
+	}
 
-  return (
-    <>
-      <Form onSubmit={handlePatchSubmit}>
-        <Form.Group controlId="date">
-          <Form.Label>Date:</Form.Label>
-          <Form.Control
-            type="date"
-            placeholder="Enter date"
-            name="date"
-            defaultValue={new Date(datum).toISOString().split("T")[0]}
-          />
-        </Form.Group>
-        <Form.Group controlId="homeTeam">
-          <Form.Label>Home Team:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter home team"
-            name="homeTeam"
-            defaultValue={thuis}
-          />
-        </Form.Group>
-        <Form.Group controlId="awayTeam">
-          <Form.Label>Away Team:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter away team"
-            name="awayTeam"
-            defaultValue={uit}
-          />
-        </Form.Group>
-        <Form.Group controlId="resultaat">
-          <Form.Label>
-            Resultaat: (1) thuisploeg gewonnen (2) uitploeg gewonnen (x)
-            gelijkspel
-          </Form.Label>
-          <Form.Control type="text" placeholder="Resultaat" name="resultaat" defaultValue={resultaat} />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </>
-  )
+	return (
+		<>
+			<Form onSubmit={handlePatchSubmit}>
+				<Form.Group controlId="date">
+					<Form.Label>Date:</Form.Label>
+					<Form.Control
+						type="date"
+						placeholder="Enter date"
+						name="date"
+						defaultValue={new Date(datum).toISOString().split("T")[0]}
+					/>
+				</Form.Group>
+				<Form.Group controlId="homeTeam">
+					<Form.Label>Home Team:</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Enter home team"
+						name="homeTeam"
+						defaultValue={thuis}
+					/>
+				</Form.Group>
+				<Form.Group controlId="awayTeam">
+					<Form.Label>Away Team:</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Enter away team"
+						name="awayTeam"
+						defaultValue={uit}
+					/>
+				</Form.Group>
+				<Form.Group controlId="resultaat">
+					<Form.Label>Result:</Form.Label>
+					<Form.Label>
+						Resultaat: (1) thuisploeg gewonnen (2) uitploeg gewonnen (x)
+						gelijkspel
+					</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Enter result"
+						name="resultaat"
+						defaultValue={resultaat}
+					/>
+				</Form.Group>
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</>
+	);
 }

31439418b20aabbe49e41686f690245e189c753c
igorseynaeve12 <igor.seynaeve@student.vives.be>
2024-10-29 10:02:21 +0100

    Disabled wedstrijdpanel fixed

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,47 +1,64 @@
-"use client"
-import React, { useEffect, useState } from "react"
+'use client';
+import React, { useEffect, useState } from 'react';
 
-import KlassementPanel from "@/components/KlassementPanel"
-import KlassementSeizoenPannel from "@/components/KlassementSeizoenPannel"
-import SeizoenPanel from "@/components/SeizoenPanel"
-import WedstrijdPanel from "@/components/WedstrijdPanel"
-import "@/styles/Home.css"
-import { getSpeeldagen } from "@/src/api_calls"
+import KlassementPanel from '@/components/KlassementPanel';
+import KlassementSeizoenPannel from '@/components/KlassementSeizoenPannel';
+import SeizoenPanel from '@/components/SeizoenPanel';
+import WedstrijdPanel from '@/components/WedstrijdPanel';
+import '@/styles/Home.css';
+import { getSpeeldagen } from '@/src/api_calls';
 
 export default function Home() {
-  const [leftPanelSelected, setLeftPanelSelected] = useState(true)
-  const [selectedSpeeldag, setSelectedSpeeldag] = useState(null)
-  const [showklassementSeizoenPanel, setShowKlassementSeizoenPanel] = useState(true)
-  const [speeldagen, setSpeeldagen] = useState([])
+  const [leftPanelSelected, setLeftPanelSelected] = useState(true);
+  const [selectedSpeeldag, setSelectedSpeeldag] = useState(null);
+  const [showklassementSeizoenPanel, setShowKlassementSeizoenPanel] =
+    useState(true);
+  const [speeldagen, setSpeeldagen] = useState([]);
 
   const onSelectSpeeldag = (itemIdx) => {
     if (selectedSpeeldag === itemIdx) {
-      setShowKlassementSeizoenPanel(true)
-      setSelectedSpeeldag(null)
+      setShowKlassementSeizoenPanel(true);
+      setSelectedSpeeldag(null);
     } else {
-      setShowKlassementSeizoenPanel(false)
-      setSelectedSpeeldag(itemIdx)
+      setShowKlassementSeizoenPanel(false);
+      setSelectedSpeeldag(itemIdx);
     }
-    //setLeftPanelSelected(false)
-  }
+  };
 
   useEffect(() => {
     getSpeeldagen()
       .then(setSpeeldagen)
-      .catch(error => console.error(error.message))
-  }, [])
+      .catch((error) => console.error(error.message));
+  }, []);
 
   return (
-    <div className="pageContainer">
-      <div className="smallColumn">
+    <div className='pageContainer'>
+      <div className='smallColumn'>
         <SeizoenPanel onClick={onSelectSpeeldag} speeldagen={speeldagen} />
       </div>
-      <div className="column flexColumn">
-        <div className="panelNav">
-          <button onClick={() => setLeftPanelSelected(true)} style={{ backgroundColor: leftPanelSelected ? "#bc6c25" : "#dda15e" }}>
+      <div className='column flexColumn'>
+        <div className='panelNav'>
+          <button
+            onClick={() => setLeftPanelSelected(true)}
+            style={{
+              backgroundColor: leftPanelSelected ? '#bc6c25' : '#dda15e',
+            }}
+          >
             Klassement
           </button>
-          <button onClick={() => setLeftPanelSelected(false)} style={{ backgroundColor: !leftPanelSelected ? "#bc6c25" : "#dda15e" }}>
+          <button
+            onClick={() =>
+              selectedSpeeldag !== null && setLeftPanelSelected(false)
+            }
+            style={{
+              backgroundColor:
+                !leftPanelSelected && selectedSpeeldag !== null
+                  ? '#bc6c25'
+                  : '#dda15e',
+              cursor: selectedSpeeldag === null ? 'not-allowed' : 'pointer',
+            }}
+            disabled={selectedSpeeldag === null}
+          >
             Wedstrijd
           </button>
         </div>
@@ -51,13 +68,20 @@ export default function Home() {
           ) : (
             <>
               {leftPanelSelected ? (
-                <KlassementPanel speeldag_id={speeldagen[selectedSpeeldag]._id} />
-              ) : <WedstrijdPanel speeldag_id={speeldagen[selectedSpeeldag]._id} />
-              }
+                <KlassementPanel
+                  speeldag_id={speeldagen[selectedSpeeldag]?._id}
+                />
+              ) : (
+                selectedSpeeldag !== null && (
+                  <WedstrijdPanel
+                    speeldag_id={speeldagen[selectedSpeeldag]?._id}
+                  />
+                )
+              )}
             </>
           )}
         </div>
       </div>
     </div>
-  )
+  );
 }

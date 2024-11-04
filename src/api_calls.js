37db87d169cc8dab42794205a8bf758041cb7d63
Tuur Keersebilck <tuur.keersebilck@student.vives.be>
2024-11-04 19:14:12 +0100

    patchwedstrijd fix

--- src/api_calls.js
+++ src/api_calls.js
@@ -2,40 +2,40 @@
 // will inline those variables in the build, removing the possiblility to obtain them
 // at runtime, TODO: make these non inlined
 
-const BASE_URL = function() {
-  const url = process.env.NEXT_PUBLIC_API_URL
-  if (!url) {
-    throw new Error("env variable NEXT_PUBLIC_API_URL must be set")
-  }
-  // URL constructor in request() requires base to have a trailing slash
-  // otherwise it ignores segments
-  return url.endsWith("/") ? url : url + "/"
-}()
+const BASE_URL = (function () {
+	const url = process.env.NEXT_PUBLIC_API_URL;
+	if (!url) {
+		throw new Error("env variable NEXT_PUBLIC_API_URL must be set");
+	}
+	// URL constructor in request() requires base to have a trailing slash
+	// otherwise it ignores segments
+	return url.endsWith("/") ? url : url + "/";
+})();
 
-const get = (extraUrl) => request("GET", extraUrl)
+const get = (extraUrl) => request("GET", extraUrl);
 
 const post = (extraUrl, bodyObj) => {
-  assert(bodyObj != null)
-  return request("POST", extraUrl, bodyObj)
-}
+	assert(bodyObj != null);
+	return request("POST", extraUrl, bodyObj);
+};
 
 const patch = (extraUrl, bodyObj) => {
-  assert(bodyObj != null)
-  return request("PATCH", extraUrl, bodyObj)
-}
+	assert(bodyObj != null);
+	return request("PATCH", extraUrl, bodyObj);
+};
 
 const put = (extraUrl, bodyObj) => {
-  assert(bodyObj != null)
-  return request("PUT", extraUrl, bodyObj)
-}
+	assert(bodyObj != null);
+	return request("PUT", extraUrl, bodyObj);
+};
 
-const delete_ = (extraUrl) => request("DELETE", extraUrl)
+const delete_ = (extraUrl) => request("DELETE", extraUrl);
 
 const assert = (cond) => {
-  if (!cond) {
-    throw new Error("assertion failed")
-  }
-}
+	if (!cond) {
+		throw new Error("assertion failed");
+	}
+};
 
 /**
  * @param {"GET" | "POST" | "PUT" | "PATCH" | "DELETE"} method
@@ -44,56 +44,62 @@ const assert = (cond) => {
  * @param {object | undefined} extraFetchOpts
  */
 const request = (method, extraUrl, bodyObj, extraFetchOpts) => {
-  if (extraUrl[0] === "/") extraUrl = extraUrl.slice(1)
-  const url = new URL(extraUrl, BASE_URL).toString()
+	if (extraUrl[0] === "/") extraUrl = extraUrl.slice(1);
+	const url = new URL(extraUrl, BASE_URL).toString();
 
-  const opts = {
-    method,
-    headers: new Headers({ "Content-Type": "application/json" }),
-    body: bodyObj != undefined ? JSON.stringify(bodyObj) : null,
-    ...extraFetchOpts,
-  }
+	const opts = {
+		method,
+		headers: new Headers({ "Content-Type": "application/json" }),
+		body: bodyObj != undefined ? JSON.stringify(bodyObj) : null,
+		...extraFetchOpts,
+	};
 
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(url, opts)
-      if (response.ok) {
-        const data = await response.json()
-        return resolve(data)
-      }
+	return new Promise(async (resolve, reject) => {
+		try {
+			const response = await fetch(url, opts);
+			if (response.ok) {
+				const data = await response.json();
+				return resolve(data);
+			}
 
-      let errorMessage = `Request to ${url} failed with code ${response.status}: `
+			let errorMessage = `Request to ${url} failed with code ${response.status}: `;
 
-      // if the response is json, try to extract the "message" key, sometimes the api returns this, sometimes it doesnt
-      if (response.headers.get("Content-Type")?.includes("application/json")) {
-        // FIXME: is this _always_ safe to call?
-        const error = await response.json()
-        errorMessage += error.message ?? error
-      } else {
-        errorMessage += response.statusText
-      }
+			// if the response is json, try to extract the "message" key, sometimes the api returns this, sometimes it doesnt
+			if (response.headers.get("Content-Type")?.includes("application/json")) {
+				// FIXME: is this _always_ safe to call?
+				const error = await response.json();
+				errorMessage += error.message ?? error;
+			} else {
+				errorMessage += response.statusText;
+			}
 
-      reject(new Error(errorMessage))
-    } catch (error) {
-      reject(new Error(`Failed to reach api(${url}): ${error.message}`, { cause: error }))
-    }
-  })
-}
+			reject(new Error(errorMessage));
+		} catch (error) {
+			reject(
+				new Error(`Failed to reach api(${url}): ${error.message}`, {
+					cause: error,
+				})
+			);
+		}
+	});
+};
 
 export const getSpeeldagen = async () => {
-  const [season] = await get("/seizoenen")
-  const speeldagen = season.speeldagen
-  speeldagen.seizoenID = season._id
-  return speeldagen
-}
+	const [season] = await get("/seizoenen");
+	const speeldagen = season.speeldagen;
+	speeldagen.seizoenID = season._id;
+	return speeldagen;
+};
 
-export const getSpeeldagenBySeizoenId = (seasonId) => get(`/seizoenen/${seasonId}/speeldagen`)
+export const getSpeeldagenBySeizoenId = (seasonId) =>
+	get(`/seizoenen/${seasonId}/speeldagen`);
 
-export const updateSpeeldagIsOnline = (speeldagId, isOnline) => put(`/speeldagen/${speeldagId}/isOnline`, { isOnline })
+export const updateSpeeldagIsOnline = (speeldagId, isOnline) =>
+	put(`/speeldagen/${speeldagId}/isOnline`, { isOnline });
 
-export const getSpeeldag = (id) => get(`/speeldagen/${id}`)
+export const getSpeeldag = (id) => get(`/speeldagen/${id}`);
 
-export const getSeizoenen = () => get("/seizoenen")
+export const getSeizoenen = () => get("/seizoenen");
 
 /**
  * Season returned from the api.
@@ -122,98 +128,135 @@ export const getSeizoenen = () => get("/seizoenen")
  * @param {SeasonInput} season
  * @returns {Promise<Season>}
  */
-export const createSeizoen = (season) => post("/seizoenen", season)
+export const createSeizoen = (season) => post("/seizoenen", season);
 
-export const getKlassementSpeeldag = (id) => get(`/speeldagen/${id}/klassement`)
+export const getKlassementSpeeldag = (id) =>
+	get(`/speeldagen/${id}/klassement`);
 
-export const getKlassementSeizoen = (seasonId) => get(`/seizoenen/${seasonId}/klassement`)
+export const getKlassementSeizoen = (seasonId) =>
+	get(`/seizoenen/${seasonId}/klassement`);
 
-export const getUser = (id) => get(`/users/${id}`)
+export const getUser = (id) => get(`/users/${id}`);
 
-export const getAllUsers = () => get("/users")
+export const getAllUsers = () => get("/users");
 
-export const updateUserBetaald = (userId, betaald) => patch(`/users/${userId}`, { betaald })
+export const updateUserBetaald = (userId, betaald) =>
+	patch(`/users/${userId}`, { betaald });
 
-export const postSpeeldagJokerAndSchiftingsAntwoord = (jokerGebruikt, schiftingsAntwoord, speeldagId) => {
-  const data = {
-    user: localStorage.getItem("userID"),
-    jokerGebruikt,
-    SchiftingsvraagAntwoord: schiftingsAntwoord,
-    wedstrijdVotes: [],
-  }
-  return post(`/speeldagVotes/${speeldagId}`, data)
-}
+export const postSpeeldagJokerAndSchiftingsAntwoord = (
+	jokerGebruikt,
+	schiftingsAntwoord,
+	speeldagId
+) => {
+	const data = {
+		user: localStorage.getItem("userID"),
+		jokerGebruikt,
+		SchiftingsvraagAntwoord: schiftingsAntwoord,
+		wedstrijdVotes: [],
+	};
+	return post(`/speeldagVotes/${speeldagId}`, data);
+};
 
 export const putSpeeldagVote = (data, speeldagId) => {
-  data.user = localStorage.getItem("userID")
-  return put(`/speeldagVotes/${speeldagId}`, data)
-}
+	data.user = localStorage.getItem("userID");
+	return put(`/speeldagVotes/${speeldagId}`, data);
+};
 
 export const postWedstrijd = (date, thuis, uit, speeldagId) => {
-  const data = { datum: date, thuis, uit }
-  return post(`/speeldagen/${speeldagId}/wedstrijden`, data)
-}
+	const data = { datum: date, thuis, uit };
+	return post(`/speeldagen/${speeldagId}/wedstrijden`, data);
+};
 
-export const patchWedstrijd = async (date, thuis, uit, resultaat, wedstrijdId, seizoenId) => {
-  const data = { datum: date, resultaat, thuis, uit }
+export const patchWedstrijd = async (
+	date,
+	thuis,
+	uit,
+	resultaat,
+	wedstrijdId,
+	seizoenId
+) => {
+	const data = { datum: date, resultaat, thuis, uit };
 
-  // cannot run those in parallel
-  try {
-    await patch(`/wedstrijden/${wedstrijdId}`, data)
-    await updateKlassementen(seizoenId)
-  } catch (error) {
-    return Promise.reject(error)
-  }
-}
+	try {
+		await patch(`/wedstrijden/${wedstrijdId}`, data);
+		console.log("patching wedstrijd with data:", data);
+
+		await Promise.all(await updateKlassementen(seizoenId)); // TODO: KAPOT
+	} catch (error) {
+		return Promise.reject(
+			new Error(
+				`Failed to patch wedstrijd or update klassement: ${error.message}`
+			)
+		);
+	}
+};
 
 // TODO: unused param seasonId
 export const updateKlassementen = async (seasonId) => {
-  const speeldagen = await getSpeeldagen()
-  return speeldagen.map(speeldag => updateSpeeldagKlassement(speeldag._id))
-}
+	const speeldagen = await getSpeeldagen();
+	return speeldagen.map((speeldag) => updateSpeeldagKlassement(speeldag._id));
+};
 
-export const updateSpeeldagKlassement = (speeldagId) => post(`/speeldagen/${speeldagId}/klassement`)
+export const updateSpeeldagKlassement = (speeldagId) =>
+	post(`/speeldagen/${speeldagId}/klassement`);
 
-export const patchSpeeldag = async (schiftingsvraag, schiftingsantwoord, startDatum, eindDatum, speeldagId) => {
-  const data = {
-    schiftingsantwoord: Number(schiftingsantwoord),
-    schiftingsvraag: schiftingsvraag,
-    startDatum: startDatum,
-    eindDatum: eindDatum
-  }
+export const patchSpeeldag = async (
+	schiftingsvraag,
+	schiftingsantwoord,
+	startDatum,
+	eindDatum,
+	speeldagId
+) => {
+	const data = {
+		schiftingsantwoord: Number(schiftingsantwoord),
+		schiftingsvraag: schiftingsvraag,
+		startDatum: startDatum,
+		eindDatum: eindDatum,
+	};
 
-  return patch(`/speeldagen/${speeldagId}`, data)
-}
+	return patch(`/speeldagen/${speeldagId}`, data);
+};
 
-export const beeindigSeizoen = (seasonId) => patch(`/seizoenen/${seasonId}`, { seizoenBeendigd: true })
+export const beeindigSeizoen = (seasonId) =>
+	patch(`/seizoenen/${seasonId}`, { seizoenBeendigd: true });
 
-export const postSpeeldag = (schiftingsvraag, schiftingsantwoord, startDatum, einddatum, seizoenId) => {
-  const data = {
-    schiftingsantwoord: Number(schiftingsantwoord),
-    schiftingsvraag: schiftingsvraag,
-    wedstrijden: [],
-    speeldagVotes: [],
-    klassement: [],
-    startDatum: startDatum,
-    eindDatum: einddatum
-  }
+export const postSpeeldag = (
+	schiftingsvraag,
+	schiftingsantwoord,
+	startDatum,
+	einddatum,
+	seizoenId
+) => {
+	const data = {
+		schiftingsantwoord: Number(schiftingsantwoord),
+		schiftingsvraag: schiftingsvraag,
+		wedstrijden: [],
+		speeldagVotes: [],
+		klassement: [],
+		startDatum: startDatum,
+		eindDatum: einddatum,
+	};
 
-  return post(`/seizoenen/${seizoenId}/speeldagen`, data)
-}
+	return post(`/seizoenen/${seizoenId}/speeldagen`, data);
+};
 
-export const deleteWedstrijd = (wedstrijdId) => delete_(`/wedstrijden/${wedstrijdId}`)
+export const deleteWedstrijd = (wedstrijdId) =>
+	delete_(`/wedstrijden/${wedstrijdId}`);
 
-export const patchSpeeldagVote = (obj, voteId) => patch(`/speeldagVotes/${voteId}`, { obj })
+export const patchSpeeldagVote = (obj, voteId) =>
+	patch(`/speeldagVotes/${voteId}`, { obj });
 
 export const getUserVotesBySpeeldagId = (speeldagId) => {
-  const loggedInUser = localStorage.getItem("userID")
-  return get(`/speeldagVotes/${speeldagId}/${loggedInUser}/votes`)
-}
+	const loggedInUser = localStorage.getItem("userID");
+	return get(`/speeldagVotes/${speeldagId}/${loggedInUser}/votes`);
+};
 
 /**
  * @returns {boolean}
  */
 export const getAdminStatus = async () => {
-  const data = await request("GET", "/auth/status", undefined, { credentials: "include" })
-  return data.isAdmin
-}
+	const data = await request("GET", "/auth/status", undefined, {
+		credentials: "include",
+	});
+	return data.isAdmin;
+};

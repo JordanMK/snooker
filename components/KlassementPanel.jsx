8fcfb3a1142bba9439ce10a48369b25b035d17da
igorseynaeve12 <igor.seynaeve@student.vives.be>
2024-11-13 15:37:13 +0100

    createspeeldag klassement werkt, maar nog bug op klassementpanel pagina

--- components/KlassementPanel.jsx
+++ components/KlassementPanel.jsx
@@ -1,98 +1,104 @@
-import React, { useState, useEffect } from "react"
-import {
-  getSpeeldagen,
-  getUser,
-  getKlassementSpeeldag,
-} from "@/src/api_calls"
-import "@/styles/Klassement.css"
-import "react-bootstrap"
+import React, { useState, useEffect } from "react";
+import { getSpeeldagen, getUser, getKlassementSpeeldag } from "@/src/api_calls";
+import "@/styles/Klassement.css";
+import "react-bootstrap";
 
 // TODO: cleanup
 export default function KlassementPanel({ speeldagId }) {
-  // TODO: unused
-  const [speeldagen, setSpeeldagen] = useState([])
-  const [speeldag, setSpeeldag] = useState([])
-  const [klassement, setKlassement] = useState([])
-  const [isLoading, setIsLoading] = useState(true) // Track loading state
+	// TODO: unused
+	const [speeldagen, setSpeeldagen] = useState([]);
+	const [speeldag, setSpeeldag] = useState([]);
+	const [klassement, setKlassement] = useState([]);
+	const [isLoading, setIsLoading] = useState(true); // Track loading state
 
-  const onMount = async () => {
-    try {
-      const speeldagen = await getSpeeldagen();
-      setSpeeldagen(speeldagen);
-      setSpeeldag(speeldagen.find((s) => s._id === speeldagId));
+	const onMount = async () => {
+		try {
+			const speeldagen = await getSpeeldagen();
+			setSpeeldagen(speeldagen);
+			setSpeeldag(speeldagen.find((s) => s._id === speeldagId));
+			console.log("SPEELDAGID: ", speeldagId);
+			const speeldagKlassement = await getKlassementSpeeldag(speeldagId);
+			console.log(
+				"Speeldag Klassement + id:",
+				speeldagId,
+				speeldagKlassement.klassement
+			);
 
-      const speeldagKlassement = await getKlassementSpeeldag(speeldagId);
-      console.log('Speeldag Klassement:', speeldagKlassement);
+			const updatedKlassement = await Promise.all(
+				speeldagKlassement.klassement.map(async (item) => {
+					const user = await getUser(item.user);
+					item.user = user.username;
+					return item;
+				})
+			);
 
-      const updatedKlassement = await Promise.all(
-        speeldagKlassement.klassement.map(async (item) => {
-          const user = await getUser(item.user);
-          item.user = user.username;
-          return item;
-        })
-      );
+			console.log("Updated Klassement:", updatedKlassement);
+			setKlassement(updatedKlassement);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-      console.log('Updated Klassement:', updatedKlassement);
-      setKlassement(updatedKlassement);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	useEffect(() => {
+		console.log("Geselecteerde speeldag ID: ", speeldagId);
+		onMount();
+	}, [speeldagId]);
 
-  useEffect(() => { onMount() }, [speeldagId])
+	// Render only when klassement is no longer undefined
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
 
-  // Render only when klassement is no longer undefined
-  if (isLoading) {
-    return <p>Loading...</p>
-  }
-
-  return (
-    <>
-      {klassement && klassement.length > 0 && (
-        <div className="">
-          <div className="panelKlassement">
-            <div className="klassementSpeeldag">
-              <h1>Klassement Speeldag</h1>
-              <p>Resultaat Schiftingsvraag: <strong>{speeldag.schiftingsantwoord}</strong></p>
-              <table className="styled-table">
-                <thead>
-                  <tr>
-                    <th>Plaats</th>
-                    <th>Naam</th>
-                    <th>Score</th>
-                    <th>Heeft joker gebruikt</th>
-                    {isBeforeToday(speeldag.eindDatum) && (<th>Antwoord SchiftingsVraag</th>)}
-                  </tr>
-                </thead>
-                <tbody>
-                  {klassement.map((item) => (
-                    <tr key={item._id}>
-                      <td>{item.plaats}</td>
-                      <td>{item.user}</td>
-                      <td>{item.score}</td>
-                      <td>{item.jokerGebruikt ? "Ja" : "Nee"}</td>
-                      {isBeforeToday(speeldag.eindDatum) && (<td>{item.SchiftingsvraagAntwoord}</td>)}
-
-                    </tr>
-
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      )}
-      {klassement && klassement.length === 0 && (
-        <p>Geen speeldag klassement beschikbaar</p>
-      )}
-
-    </>
-  )
+	return (
+		<>
+			{klassement && klassement.length > 0 && (
+				<div className="">
+					<div className="panelKlassement">
+						<div className="klassementSpeeldag">
+							<h1>Klassement Speeldag</h1>
+							<p>
+								Resultaat Schiftingsvraag:{" "}
+								<strong>{speeldag.schiftingsantwoord}</strong>
+							</p>
+							<table className="styled-table">
+								<thead>
+									<tr>
+										<th>Plaats</th>
+										<th>Naam</th>
+										<th>Score</th>
+										<th>Heeft joker gebruikt</th>
+										{isBeforeToday(speeldag.eindDatum) && (
+											<th>Antwoord SchiftingsVraag</th>
+										)}
+									</tr>
+								</thead>
+								<tbody>
+									{klassement.map((item) => (
+										<tr key={item._id}>
+											<td>{item.plaats}</td>
+											<td>{item.user}</td>
+											<td>{item.score}</td>
+											<td>{item.jokerGebruikt ? "Ja" : "Nee"}</td>
+											{isBeforeToday(speeldag.eindDatum) && (
+												<td>{item.SchiftingsvraagAntwoord}</td>
+											)}
+										</tr>
+									))}
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			)}
+			{klassement && klassement.length === 0 && (
+				<p>Geen speeldag klassement beschikbaar</p>
+			)}
+		</>
+	);
 }
 
 function isBeforeToday(datum) {
-  return new Date(datum) < new Date()
+	return new Date(datum) < new Date();
 }
-
--- src/app/admin/speeldagen/page.jsx
+++ src/app/admin/speeldagen/page.jsx
@@ -1,266 +1,285 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-import AdminPopup from '@/components/Popup';
-import SpeelDagForm from '@/components/admin/speeldag/CreateSpeeldagForm';
-import WedstrijdForm from '@/components/admin/wedstrijd/CreateWedstrijd';
-import WedstrijdAdmin from '@/components/admin/wedstrijd/wedstrijdAdmin';
+import AdminPopup from "@/components/Popup";
+import SpeelDagForm from "@/components/admin/speeldag/CreateSpeeldagForm";
+import WedstrijdForm from "@/components/admin/wedstrijd/CreateWedstrijd";
+import WedstrijdAdmin from "@/components/admin/wedstrijd/wedstrijdAdmin";
 
 import {
-  getSpeeldagen,
-  patchSpeeldag,
-  updateKlassementen,
-  getSpeeldagenBySeizoenId,
-  updateSpeeldagIsOnline,
-  getAdminStatus,
-} from '@/src/api_calls';
+	getSpeeldagen,
+	patchSpeeldag,
+	updateKlassementen,
+	getSpeeldagenBySeizoenId,
+	updateSpeeldagIsOnline,
+	getAdminStatus,
+	updateSpeeldagKlassement,
+} from "@/src/api_calls";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'reactjs-popup/dist/index.css';
-import '@/styles/style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "reactjs-popup/dist/index.css";
+import "@/styles/style.css";
 
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button } from "react-bootstrap";
 
 export default function Speeldagen() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const seizoenId = searchParams.get('seizoenId');
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const seizoenId = searchParams.get("seizoenId");
 
-  const [speeldagen, setSpeeldagen] = useState([]);
-  const [isAdmin, setIsAdmin] = useState(false)
+	const [speeldagen, setSpeeldagen] = useState([]);
+	const [isAdmin, setIsAdmin] = useState(false);
 
-  const checkAuthState = async () => {
-    const isAdmin = await getAdminStatus()
-    if (isAdmin) {
-      setIsAdmin(isAdmin)
+	const checkAuthState = async () => {
+		const isAdmin = await getAdminStatus();
+		if (isAdmin) {
+			setIsAdmin(isAdmin);
 
-      getSpeeldagenBySeizoenId(seizoenId)
-        .then(setSpeeldagen)
-        .catch(console.error);
-    } else {
-      router.push("/")
-    }
-  }
+			getSpeeldagenBySeizoenId(seizoenId)
+				.then(setSpeeldagen)
+				.catch(console.error);
+		} else {
+			router.push("/");
+		}
+	};
 
-  useEffect(() => { checkAuthState() }, [])
+	useEffect(() => {
+		checkAuthState();
+	}, []);
 
-  if (!isAdmin) return null
+	if (!isAdmin) return null;
 
-  const updateIsOnline = async (speeldagId, online) => {
-    try {
-      await updateSpeeldagIsOnline(speeldagId, online); // Update de speeldag
-      const updatedSpeeldagen = await getSpeeldagen(); // Haal opnieuw alle speeldagen op
-      setSpeeldagen(updatedSpeeldagen); // Update de status in de UI
-    } catch (error) {
-      console.error('Error updating speeldag:', error);
-    }
-  }
+	const updateIsOnline = async (speeldagId, online) => {
+		try {
+			await updateSpeeldagIsOnline(speeldagId, online); // Update de speeldag
+			const updatedSpeeldagen = await getSpeeldagen(); // Haal opnieuw alle speeldagen op
+			setSpeeldagen(updatedSpeeldagen); // Update de status in de UI
+		} catch (error) {
+			console.error("Error updating speeldag:", error);
+		}
+	};
 
-  return (
-    <>
-      <div className='header'>
-        <h1>Admin Dashboard</h1>
-      </div>
-      <AdminPopup triggerButtonName='Nieuwe speeldag'>
-        <SpeelDagForm />
-      </AdminPopup>
-      {speeldagen.length > 0 && (
-        <div className='speeldag'>
-          <ul>
-            {speeldagen
-              .slice()
-              .reverse()
-              .map((speeldag, idx) => (
-                <Speeldag
-                  speeldag={speeldag}
-                  number={speeldagen.length - idx}
-                  updateIsOnline={updateIsOnline}
-                  key={speeldag._id}
-                />
-              ))}
-          </ul>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<div className="header">
+				<h1>Admin Dashboard</h1>
+			</div>
+			<AdminPopup triggerButtonName="Nieuwe speeldag">
+				<SpeelDagForm />
+			</AdminPopup>
+			{speeldagen.length > 0 && (
+				<div className="speeldag">
+					<ul>
+						{speeldagen
+							.slice()
+							.reverse()
+							.map((speeldag, idx) => (
+								<Speeldag
+									speeldag={speeldag}
+									number={speeldagen.length - idx}
+									updateIsOnline={updateIsOnline}
+									key={speeldag._id}
+								/>
+							))}
+					</ul>
+				</div>
+			)}
+		</>
+	);
 }
 
 function Speeldag({ speeldag, number, updateIsOnline }) {
-  const {
-    schiftingsvraag,
-    schiftingsantwoord,
-    startDatum,
-    eindDatum,
-    _id,
-    wedstrijden,
-    isOnline,
-  } = speeldag;
+	const {
+		schiftingsvraag,
+		schiftingsantwoord,
+		startDatum,
+		eindDatum,
+		_id,
+		wedstrijden,
+		isOnline,
+	} = speeldag;
 
-  const onCheckboxChange = (event) => {
-    updateIsOnline(_id, event.target.checked)
-  }
+	const onCheckboxChange = (event) => {
+		updateIsOnline(_id, event.target.checked);
+	};
 
-  return (
-    <li>
-      <div className='speeldagHead'>
-        <h2>Speeldag {number}</h2>
+	const onCreateKlassementButtonPressed = (event) => {
+		try {
+			updateSpeeldagKlassement(_id);
+			console.log(speeldag.klassement);
+		} catch (error) {
+			console.error("Error creating seizoensklassement:", error);
+		}
+	};
 
-        <AdminPopup triggerButtonName='Pas aan'>
-          <PasSpeeldagAan
-            schiftingsvraag={schiftingsvraag}
-            schiftingsantwoord={schiftingsantwoord}
-            startDatum={startDatum}
-            eindDatum={eindDatum}
-            speeldagId={_id}
-          />
-        </AdminPopup>
+	return (
+		<li>
+			<div className="speeldagHead">
+				<h2>Speeldag {number}</h2>
 
-        <AdminPopup triggerButtonName='Nieuwe wedstrijd'>
-          <WedstrijdForm id={_id} />
-        </AdminPopup>
+				<AdminPopup triggerButtonName="Pas aan">
+					<PasSpeeldagAan
+						schiftingsvraag={schiftingsvraag}
+						schiftingsantwoord={schiftingsantwoord}
+						startDatum={startDatum}
+						eindDatum={eindDatum}
+						speeldagId={_id}
+					/>
+				</AdminPopup>
 
-        <label htmlFor='online'>Plaats speeldag online:</label>
-        <input
-          type='checkbox'
-          name='online'
-          checked={isOnline}
-          onChange={onCheckboxChange}
-        />
-      </div>
-      {wedstrijden.length > 0 && <WedstrijdAdmin wedstrijden={wedstrijden} />}
-    </li>
-  );
+				<AdminPopup triggerButtonName="Nieuwe wedstrijd">
+					<WedstrijdForm id={_id} />
+				</AdminPopup>
+
+				<label htmlFor="online">Plaats speeldag online:</label>
+				<input
+					type="checkbox"
+					name="online"
+					checked={isOnline}
+					onChange={onCheckboxChange}
+				/>
+
+				<input
+					type="button"
+					name="Klassement"
+					value="Creer Klassement"
+					onClick={onCreateKlassementButtonPressed}
+				/>
+			</div>
+			{wedstrijden.length > 0 && <WedstrijdAdmin wedstrijden={wedstrijden} />}
+		</li>
+	);
 }
 
 function PasSpeeldagAan({
-  schiftingsvraag,
-  schiftingsantwoord,
-  startDatum,
-  eindDatum,
-  speeldagId,
+	schiftingsvraag,
+	schiftingsantwoord,
+	startDatum,
+	eindDatum,
+	speeldagId,
 }) {
-  const searchParams = useSearchParams();
-  const seizoenId = searchParams.get('seizoenId');
+	const searchParams = useSearchParams();
+	const seizoenId = searchParams.get("seizoenId");
 
-  const formattedStartDatum = formattedDate(new Date(startDatum));
-  const formattedStartUur = formattedTime(new Date(startDatum));
+	const formattedStartDatum = formattedDate(new Date(startDatum));
+	const formattedStartUur = formattedTime(new Date(startDatum));
 
-  const formattedEindDatum = formattedDate(new Date(eindDatum));
-  const formattedEindUur = formattedTime(new Date(eindDatum));
+	const formattedEindDatum = formattedDate(new Date(eindDatum));
+	const formattedEindUur = formattedTime(new Date(eindDatum));
 
-  function handlePatchSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const startdatum = formData.get('startdatum');
-    const startUur = formData.get('startUur');
-    const eindDatum = formData.get('eindDatum');
-    const eindUur = formData.get('einduur');
-    const schiftingsvraag = formData.get('schiftingsvraag');
-    const schiftingantwoord = formData.get('schiftingantwoord');
+	function handlePatchSubmit(event) {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		const startdatum = formData.get("startdatum");
+		const startUur = formData.get("startUur");
+		const eindDatum = formData.get("eindDatum");
+		const eindUur = formData.get("einduur");
+		const schiftingsvraag = formData.get("schiftingsvraag");
+		const schiftingantwoord = formData.get("schiftingantwoord");
 
-    const startDate = new Date(startdatum + ' ' + startUur).toISOString();
-    const eindDate = new Date(eindDatum + ' ' + eindUur).toISOString();
+		const startDate = new Date(startdatum + " " + startUur).toISOString();
+		const eindDate = new Date(eindDatum + " " + eindUur).toISOString();
 
-    patchSpeeldag(
-      schiftingsvraag,
-      schiftingantwoord,
-      startDate,
-      eindDate,
-      speeldagId
-    )
-      .then(() => {
-        updateKlassementen(seizoenId);
-        setTimeout(() => {
-          window.location.reload();
-        }, 1000);
-      })
-      .catch((error) => {
-        // Handle error, if needed
-        console.error('Failed to patch wedstrijd:', error.message);
-      });
-  }
+		patchSpeeldag(
+			schiftingsvraag,
+			schiftingantwoord,
+			startDate,
+			eindDate,
+			speeldagId
+		)
+			.then(() => {
+				updateKlassementen(seizoenId);
+				setTimeout(() => {
+					window.location.reload();
+				}, 1000);
+			})
+			.catch((error) => {
+				// Handle error, if needed
+				console.error("Failed to patch wedstrijd:", error.message);
+			});
+	}
 
-  return (
-    <>
-      <Form onSubmit={handlePatchSubmit}>
-        <Form.Group controlId='schiftingsVraag'>
-          <Form.Label>Schiftingsvraag:</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Schiftingsvraag'
-            name='schiftingsvraag'
-            defaultValue={schiftingsvraag}
-          />
-        </Form.Group>
-        <Form.Group controlId='schiftingsAntwoord'>
-          <Form.Label>Schiftingsantwoord:</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Schiftingsantwoord'
-            name='schiftingantwoord'
-            defaultValue={schiftingsantwoord}
-          />
-        </Form.Group>
-        <Form.Group controlId='startdatum'>
-          <Form.Label>Start datum:</Form.Label>
-          <Form.Control
-            type='date'
-            placeholder='startdatum'
-            name='startdatum'
-            defaultValue={formattedStartDatum}
-          />
-        </Form.Group>
-        <Form.Group controlId='startUur'>
-          <Form.Label>Start uur:</Form.Label>
-          <Form.Control
-            type='time'
-            placeholder='startUur'
-            name='startUur'
-            defaultValue={formattedStartUur}
-            format='HH:mm'
-          />
-        </Form.Group>
-        <Form.Group controlId='eindDatum'>
-          <Form.Label>Eind datum:</Form.Label>
-          <Form.Control
-            type='date'
-            placeholder='eindDatum'
-            name='eindDatum'
-            defaultValue={formattedEindDatum}
-          />
-        </Form.Group>
-        <Form.Group controlId='einduur'>
-          <Form.Label>Eind uur:</Form.Label>
-          <Form.Control
-            type='time'
-            placeholder='einduur'
-            name='einduur'
-            defaultValue={formattedEindUur}
-          />
-        </Form.Group>
-        <Button variant='primary' type='submit'>
-          Submit
-        </Button>
-      </Form>
-    </>
-  );
+	return (
+		<>
+			<Form onSubmit={handlePatchSubmit}>
+				<Form.Group controlId="schiftingsVraag">
+					<Form.Label>Schiftingsvraag:</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Schiftingsvraag"
+						name="schiftingsvraag"
+						defaultValue={schiftingsvraag}
+					/>
+				</Form.Group>
+				<Form.Group controlId="schiftingsAntwoord">
+					<Form.Label>Schiftingsantwoord:</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Schiftingsantwoord"
+						name="schiftingantwoord"
+						defaultValue={schiftingsantwoord}
+					/>
+				</Form.Group>
+				<Form.Group controlId="startdatum">
+					<Form.Label>Start datum:</Form.Label>
+					<Form.Control
+						type="date"
+						placeholder="startdatum"
+						name="startdatum"
+						defaultValue={formattedStartDatum}
+					/>
+				</Form.Group>
+				<Form.Group controlId="startUur">
+					<Form.Label>Start uur:</Form.Label>
+					<Form.Control
+						type="time"
+						placeholder="startUur"
+						name="startUur"
+						defaultValue={formattedStartUur}
+						format="HH:mm"
+					/>
+				</Form.Group>
+				<Form.Group controlId="eindDatum">
+					<Form.Label>Eind datum:</Form.Label>
+					<Form.Control
+						type="date"
+						placeholder="eindDatum"
+						name="eindDatum"
+						defaultValue={formattedEindDatum}
+					/>
+				</Form.Group>
+				<Form.Group controlId="einduur">
+					<Form.Label>Eind uur:</Form.Label>
+					<Form.Control
+						type="time"
+						placeholder="einduur"
+						name="einduur"
+						defaultValue={formattedEindUur}
+					/>
+				</Form.Group>
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</>
+	);
 }
 
 function formattedDate(date) {
-  const localDate = new Date(date);
-  const offset = localDate.getTimezoneOffset(); // Get the timezone offset in minutes
-  const localISODate = new Date(
-    localDate.getTime() - offset * 60000
-  ).toISOString();
-  return localISODate.split('T')[0];
+	const localDate = new Date(date);
+	const offset = localDate.getTimezoneOffset(); // Get the timezone offset in minutes
+	const localISODate = new Date(
+		localDate.getTime() - offset * 60000
+	).toISOString();
+	return localISODate.split("T")[0];
 }
 
 function formattedTime(date) {
-  const localDate = new Date(date);
-  const offset = localDate.getTimezoneOffset(); // Get the timezone offset in minutes
-  const localISOTime = new Date(
-    localDate.getTime() - offset * 60000
-  ).toISOString();
-  return localISOTime.split('T')[1].split('.')[0];
+	const localDate = new Date(date);
+	const offset = localDate.getTimezoneOffset(); // Get the timezone offset in minutes
+	const localISOTime = new Date(
+		localDate.getTime() - offset * 60000
+	).toISOString();
+	return localISOTime.split("T")[1].split(".")[0];
 }

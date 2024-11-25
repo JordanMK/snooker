1724be791523da2532dfa117cadfdc07fa81357d
Jordan Malekera <115084133+JordanMK@users.noreply.github.com>
2024-11-25 03:03:41 +0100

    fix: resolve error on frontend page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const path = require("path")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
